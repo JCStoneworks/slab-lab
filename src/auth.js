4b2af014779b6ef66c2114349e428aebfaf2194e
JCStoneworks <allyse@jcstoneworks.net>
2026-08-28 15:41:31 -0700

    Create auth.js

---
+++ src/auth.js
@@ -0,0 +1,19 @@
+import { supabase } from "./supabaseClient.js";
+
+export async function signIn(email, password) {
+  return supabase.auth.signInWithPassword({ email, password });
+}
+
+export async function signOut() {
+  return supabase.auth.signOut();
+}
+
+export async function getSession() {
+  const { data } = await supabase.auth.getSession();
+  return data.session;
+}
+
+export function onAuthStateChange(callback) {
+  const { data } = supabase.auth.onAuthStateChange((_event, session) => callback(session));
+  return data.subscription;
+}
